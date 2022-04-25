f4f74cc4469be41f3aa6f42d2fa7643043728325
STIVVVV <stevencanfield2000@gmail.com>
2022-04-25 01:28:06 -0400

    Made a static class called global config Made an interface to handle dealing with Data Made an sql connector class

---
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitalizaConections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                //ToDo - Create the SQL Connection
+            }
+
+            if (textFiles)
+            {
+                //ToDo - Create the Text Connection
+            }
+        }
+    }
+}
